e7f0bec02cb2bc50c5c7cca1530893db4f882d62
Max Del <max.del.edu@gmail.com>
2017-12-10 07:43:50 +0200

    Add basic test for dataset reader

---
+++ tests/dataset_readers/spooky_authors_dataset_reader_test.py
@@ -0,0 +1,34 @@
+from allennlp.common.testing import AllenNlpTestCase
+from spooky-author-identification.dataset_readers import SpookyAuthorsDatasetReader
+
+class TestSpookyAuthorsDatasetReader(AllenNlpTestCase):
+    def test_read_from_file(self):
+        reader = SpookyAuthorsDatasetReader()
+        dataset = reader.read('tests/fixtures/spooky_lines.txt')
+
+        cold is merely nothing .@EAP
+        but it is not to this fact that I now especially advert .@EAP
+        no father could claim the gratitude of his child so completely as I should deserve theirs .@MWS
+
+        instance1 = {"sentence": ["the", "cold", "is", "merely", "nothing"],
+                     "author": "EAP"}
+
+        instance2 = {"sentence": ["but", "it", "is", "not", "to", 'this', 'fact', 'that', 'I', 'now',
+                     'especially', 'advert', '.'],
+                     "author": "EAP"}
+
+        instance3 = {"sentence": ["no", 'father', 'could', 'claim', 'the', 'gratitude', 'of', 'his',
+                     'child', 'so', 'completely', 'as', 'I', "should", "deserve", 'theirs', '.'],
+                     "author": "MWS"}
+
+        assert len(dataset.instances) == 6
+        fields = dataset.instances[0].fields
+        assert [t.text for t in fields["sentence"].tokens] == instance1["sentence"]
+        assert fields["label"].label == instance1["author"]
+        fields = dataset.instances[1].fields
+        assert [t.text for t in fields["sentence"].tokens[:13]] == instance2["sentence"]
+        assert fields["label"].label == instance2["author"]
+        fields = dataset.instances[2].fields
+        assert [t.text for t in fields["sentence"].tokens[:17]] == instance3["sentence"]
+        assert fields["label"].label == instance3["author"]
+
