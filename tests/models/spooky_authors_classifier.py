739bc8a295ee9f796960a5a3163ac6a06f0147c0
Max Del <max.del.edu@gmail.com>
2017-12-10 08:33:35 +0200

    Added basic model test case for the model

---
+++ tests/models/spooky_authors_classifier.py
@@ -0,0 +1,12 @@
+# pylint: disable=invalid-name,protected-access
+from allennlp.common.testing import ModelTestCase
+
+
+class SpookyAuthorsClassifierTest(ModelTestCase):
+    def setUp(self):
+        super(SpookyAuthorsClassifierTest, self).setUp()
+        self.set_up_model('tests/fixtures/spooky_authors_classifier.json',
+                          'tests/fixtures/spooky_lines.txt')
+
+    def test_model_can_train_save_and_load(self):
+        self.ensure_model_can_train_save_and_load(self.param_file)
